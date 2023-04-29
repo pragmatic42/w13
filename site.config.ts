e09209e7e3a26ca263aa30572b9a580e6dc7eabc
pragmatic42 <95586814+pragmatic42@users.noreply.github.com>
2023-04-29 18:32:40 +0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,25 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7167d2cf133a4875b7e23caf545ee402',
+  rootNotionPageId: 'c3fba8c077f743149fe8f166165af4ed',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'wr8 Demo',
-  domain: 'demo.wr8.in',
-  author: 'Verfasor',
-  seotitle: 'Demo Site Created with wr8',
+  name: 'Webinarrative',
+  domain: 'webinarrative',
+  author: 'Webinarrative',
+  seotitle: 'Webinarrative — Master Webinars 2023, Get Started, Host & Maximize Your Impact',
 
   // open graph metadata (optional)
   description: 'This is a demo website created with wr8.',
 
   // social usernames (optional)
-  twitter: 'verfasor',
-  github: 'verfasor',
-  linkedin: 'verfasor',
+  twitter: 'webinarrative',
+  // github: '',
+  // linkedin: '',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -54,15 +54,15 @@ export default siteConfig({
   navigationLinks: [   
     {
       title: 'Blog',
-      pageId: '588031c1e65d4dfa9d58e2492c9c1b7e'
+      pageId: 'd0da325342174ad0a641453bc899beda'
     },    
     {
       title: 'About',
-      pageId: 'cf2ae3c4839e4e09a1b4a33cc600c409'
+      pageId: '3eaacb6d5f7048648acc7f87544aa3fc'
     },
     {
       title: 'Contact',
-      pageId: '5f7e562fef814299b10c68c52e920c9a'
+      pageId: 'e79a170b7f95486e917f0d04e7dad518'
     }
   ]
 })
